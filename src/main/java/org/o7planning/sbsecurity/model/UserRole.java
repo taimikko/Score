0a626f9b6b0fe358b15a723da60b4559c7d95f66
Mikko Saarinen <mikkosaarinen@eduix.it>
2018-09-17 12:46:23 +0300

    Initial Commit

---
+++ src/main/java/org/o7planning/sbsecurity/model/UserRole.java
@@ -0,0 +1,52 @@
+package org.o7planning.sbsecurity.model;
+
+public class UserRole {
+	private Long id;
+	private Long userId;
+	private Long roleId;
+
+	public UserRole() {
+
+	}
+
+	public UserRole(Long id, Long userId, Long roleId) {
+		this.id = id;
+		this.userId = userId;
+		this.roleId = roleId;
+	}
+
+	public UserRole(Long userId, Long roleId) {
+		this.id = 0L;
+		this.userId = userId;
+		this.roleId = roleId;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+
+	public Long getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(Long roleId) {
+		this.roleId = roleId;
+	}
+
+	@Override
+	public String toString() {
+		return Long.toString(this.id) + ":" + Long.toString(this.userId) + " " + Long.toString(this.roleId);
+	}
+}
