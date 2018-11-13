324e8627a91151587df2db766708878c9e57b6e4
Mikko Saarinen <mikkosaarinen@eduix.it>
2018-11-13 09:46:26 +0200

    uusiTasoitus ja tasoitusMuunnos -funktiot lisätty. Ei käytetä vielä missään.

--- src/main/resources/static/js/kierros.js
+++ src/main/resources/static/js/kierros.js
@@ -275,6 +275,92 @@ function tiiChange() {
     paivitaVaylatnaytolle();
 }
 
+function laskeTasoitusMuutos(tasoitus, pisteet, kokokierros) { // todo: CBA tänne ??
+	var bufZone;
+	if (kokokierros) {
+		if (pisteet >= 36) {
+			muutos = 36 - pisteet; // laskee x 
+		} else {
+			if (tasoitus > 36) {
+				bufZone = 0;
+			} else if (tasoitus > 26,5) {
+				bufZone = 31;
+			} else if (tasoitus > 18,5) {
+				bufZone = 32;
+			} else if (tasoitus > 11,5) {
+				bufZone = 33;
+			} else if (tasoitus > 4,5) {
+				bufZone = 34;
+			} else  {
+				bufZone = 35;
+			}
+			if (pisteet < bufZone) 
+				muutos = 1; // nousee
+			else
+				muutos = 0;			
+		}
+	} else {
+		// 9 reikää
+		if (tasoitus < 4.5) { // ykkösryhmässä ei voi muuttua 9 reiän perusteella
+			muutos = 0;
+		} else {
+			pisteet += 18; // takaysi pelattu tasoitukseen
+			if (pisteet >= 36) {
+				muutos = 36 - pisteet; // laskee x 
+			} else {
+				if (tasoitus > 36) {
+					bufZone = 0;
+				} else if (tasoitus > 26,5) {
+					bufZone = 33;
+				} else if (tasoitus > 18,5) {
+					bufZone = 34;
+				} else if (tasoitus > 11,5) {
+					bufZone = 35;
+				} else if (tasoitus > 4,5) {
+					bufZone = 35;
+				}
+				if (pisteet < bufZone) 
+					muutos = 1; // nousee
+				else
+					muutos = 0;			
+			}
+		}
+	}
+}
+
+function uusiTasoitus(tasoitus, muutos) {
+/*
+HCP-ryhmä 	HCP 		BZ 18r 	BZ 9r 	nousee 	alenee
+	1	 	plus–4,4 	35–36 	– 		0,1 	0,1
+	2	 	4,5–11,4 	34–36 	35–36 	0,1 	0,2
+	3	 	11,5–18,4 	33–36 	35–36 	0,1 	0,3
+	4	 	18,5–26,4 	32–36 	34–36 	0,1 	0,4
+	5	 	26,5–36,0 	31–36 	33–36 	0,1 	0,5
+	6	 	37–54 		–	 	–	 	– 		1,0
+*/
+	if (muutos > 0) {
+		if (tasoitus < 36) tasoitus += 0.1;
+	} else if (muutos < 0) {
+		while (muutos > 0) {
+			if (tasoitus > 36) {
+				tasoitus--;
+			} else if (tasoitus > 26,5) {
+				tasoitus -= 0.5;
+			} else if (tasoitus > 18,5) {
+				tasoitus -= 0.4;
+			} else if (tasoitus > 11,5) {
+				tasoitus -= 0.3;
+			} else if (tasoitus > 4,5) {
+				tasoitus -= 0.2;
+			} else  {
+				tasoitus -= 0.1;
+			}
+			muutos++;
+		}
+	}
+	return tasoitus;
+}
+
 async function tasoitusChange() {
     const kentta_id = document.getElementById('kentta_id').value;
     const tii_id = document.getElementById('tii_id').value;
